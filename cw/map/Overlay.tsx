1d014f0dfb61d9b023d969cc75f71c83c78025cc
Lvov Alexandr <strangeisdoctor@gmail.com>
2025-12-04 23:32:28 +0300

    cw: images

--- cw/map/Overlay.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.8" tiledversion="1.8.6" name="Overlay" tilewidth="32" tileheight="32" tilecount="2072" columns="56">
- <image source="Overlay.png" width="1800" height="1200"/>
-</tileset>
--- cw/map/Tileset.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.8" tiledversion="1.8.6" name="Tileset" tilewidth="32" tileheight="32" tilecount="81" columns="9">
- <image source="Tileset.png" width="288" height="288"/>
-</tileset>
